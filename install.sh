d80d334b5e048b38e5c63cdcd9b1599eedd1d4f1
Richard Kendall <richardjkendall@users.noreply.github.com>
2021-04-19 09:41:29 +1000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+
+check_output=`echo "$(<kubectl.sha256) kubectl" | sha256sum --check`
+okay_string="kubectl: OK"
+
+if [ "$check_output" == "$okay_string" ]; then
+  echo "OKAY: downloaded file matches the expected checksum"
+  
+else
+  echo "ERROR: checksum does not match"
+  exit 1
+fi
